4b0e7d3a70e9e39d912a049a83bf97aed8068e5f
Dr. Kang <130517606+project-neo-share@users.noreply.github.com>
2025-07-31 03:47:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-
+import platform
 from factor_analyzer import FactorAnalyzer
 from sklearn.preprocessing import StandardScaler
 
